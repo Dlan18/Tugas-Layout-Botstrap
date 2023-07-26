b329e6c115012e75c5e94816defeff7b15a35ef6
Dlan18 <fkamil502@gmail.com>
2023-07-27 06:41:46 +0700

    Test

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ $conn->close();
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>SMKN 10 JKT - RANKING XI RPL</title>
+    <title>SMK N 10 JKT - RANKING XI RPL</title>
     <link rel="icon" href="logo.jpeg" />
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
